3c81ef81c4b909ec524bcc632deb6063185d09c8
mserino <m.serino@hotmail.it>
2014-10-07 19:15:00 +0200

    Analyzer finds pattern from email

--- lib/analyzer.rb
+++ lib/analyzer.rb
@@ -1,8 +1,17 @@
 class Analyzer
 
+	def get(email)
+		email.split('@')[0]
+	end
+
 	def find(string)
 		@name, @lastname = string.split('.')
 		[name_pattern, lastname_pattern].join('_dot_').to_sym
+	end
+
+	def convert(pattern)
+		string = get(pattern)
+		find(string)
 	end
 
 	private
--- spec/analyzer_spec.rb
+++ spec/analyzer_spec.rb
@@ -3,7 +3,7 @@ require 'analyzer'
 describe Analyzer do
 	let(:analyzer) { Analyzer.new }
 
-	context 'finding patterns' do
+	context 'finding patterns from simple string' do
 		it 'finds first name and last name' do
 			expect(analyzer.find('john.ferguson')).to eq :first_name_dot_last_name
 		end
@@ -20,4 +20,27 @@ describe Analyzer do
 			expect(analyzer.find('j.f')).to eq :first_initial_dot_last_initial
 		end
 	end
+
+	context 'finding patterns from email address' do
+		it 'finds the first part' do
+			expect(analyzer.get('john.ferguson@alphasights.com')).to eq 'john.ferguson'
+		end
+
+		it 'finds first name and last name from email' do
+			expect(analyzer.convert('john.ferguson@alphasights.com')).to eq :first_name_dot_last_name
+		end
+
+		it 'finds first initial and last name from email' do
+			expect(analyzer.convert('j.ferguson@alphasights.com')).to eq :first_initial_dot_last_name
+		end
+
+		it 'finds first name and last initial from email' do
+			expect(analyzer.convert('john.f@alphasights.com')).to eq :first_name_dot_last_initial
+		end
+
+		it 'finds first initial and last initial from email' do
+			expect(analyzer.convert('j.f@alphasights.com')).to eq :first_initial_dot_last_initial
+		end
+ 	end
+
 end
